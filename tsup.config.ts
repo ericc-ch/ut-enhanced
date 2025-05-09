6a36645f2f33b158caf7824cd1ef5104afc37ae8
Erick Christian <54889298+ericc-ch@users.noreply.github.com>
2025-05-10 01:34:23 +0700

    Initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+  entry: ["src/main.ts", "src/cli.ts"],
+
+  format: ["esm"],
+  target: "esnext",
+  platform: "node",
+
+  dts: true,
+  removeNodeProtocol: false,
+  sourcemap: true,
+  clean: true,
+})
