ef0d29afe1cd5375aaa654769d0e0fd3ba474068
Erick Christian <erickchristian48@gmail.com>
2025-05-15 02:12:32 +0700

    extract meta config

---
+++ meta.config.js
@@ -0,0 +1,22 @@
+/**
+ * ViolentMonkey meta builder. Used as user script banner
+ * @param {Record<string, string>} record
+ */
+function metaBuilder(record) {
+  let content = ""
+
+  for (const [key, value] of Object.entries(record)) {
+    content += `// @${key} ${value}\n`
+  }
+
+  return "// ==UserScript==\n" + content + "// ==/UserScript==\n"
+}
+
+export default metaBuilder({
+  name: "ut-enhanced",
+  namespace: "Violentmonkey Scripts",
+  description: "Make elearning.ut.ac.id better.",
+  match: "https://elearning.ut.ac.id/*",
+  version: "0.0.0",
+  author: "Erick Christian",
+})
