6faf630e4dc845927e55b8d950a247e1e9059ca1
Erick Christian <erickchristian48@gmail.com>
2025-05-15 20:31:03 +0700

    remove rolldown, uno autocomplete workaround

--- uno.config.ts
+++ uno.config.ts
@@ -4,6 +4,12 @@ export default defineConfig({
   presets: [presetWind3()],
 
   content: {
-    filesystem: ["src/**/*.ts"],
+    filesystem: ["./src/**/*.ts"],
+
+    // We're not using pipeline config since everything is handled by PostCSS
+    // But this is needed in order for VSCode autocompletion to work
+    pipeline: {
+      include: [/\.(ts|html)($|\?)/],
+    },
   },
 })
